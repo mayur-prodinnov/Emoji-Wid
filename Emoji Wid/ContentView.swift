35caf7b9334a1307bb956e516df6842e29ad9c8f
zephyrwala <mayur@redphoenix.co.in>
2020-12-04 07:42:57 +0530

    Initial Commit

---
+++ Emoji Wid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emoji Wid
+//
+//  Created by Red Phoenix on 04/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emoji Wid/Emoji_WidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_WidApp.swift
+//  Emoji Wid
+//
+//  Created by Red Phoenix on 04/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_WidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
